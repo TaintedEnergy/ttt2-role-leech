dae1c4c8e7a26fc94d2aef6fb9585a3013bd202c
EntranceJew <EntranceJew@gmail.com>
2024-01-26 00:30:27 -0600

    ULX dies, put those things in the F1 menu

--- lua/terrortown/entities/roles/leech/shared.lua
+++ lua/terrortown/entities/roles/leech/shared.lua
@@ -1,115 +1,171 @@
 if SERVER then
-  AddCSLuaFile()
-  resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_leech.vmt")
+	AddCSLuaFile()
+	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_leech.vmt")
 end
 
+local flags = {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED}
+CreateConVar("ttt2_leech_refill_radius", 50000.0, flags)
+CreateConVar("ttt2_leech_refill_multiplier", 5.0, flags)
+CreateConVar("ttt2_leech_starve_time", 30.0, flags)
+CreateConVar("ttt2_leech_tick_length", 0.02, flags)
+
 function ROLE:PreInitialize()
-  self.color = Color(187, 156, 155, 255)
+	self.color = Color(187, 156, 155, 255)
 
-  self.abbr = "leech"
-  self.surviveBonus = 0
-  self.scoreKillsMultiplier = 5
-  self.scoreTeamKillsMultiplier = -16
+	self.abbr = "leech"
+	self.surviveBonus = 0
+	self.scoreKillsMultiplier = 5
+	self.scoreTeamKillsMultiplier = -16
 
-  self.defaultEquipment = SPECIAL_EQUIPMENT
-  self.defaultTeam = TEAM_NONE
+	self.defaultEquipment = SPECIAL_EQUIPMENT
+	self.defaultTeam = TEAM_NONE
 
-  self.conVarData = {
-    pct = 0.17,
-    maximum = 1,
-    minPlayers = 6,
-    togglable = true
-  }
+	self.conVarData = {
+		pct = 0.17,
+		maximum = 1,
+		minPlayers = 6,
+		togglable = true
+	}
 end
 
 function ROLE:Initialize()
-  if SERVER and JESTER then self.networkRoles = {JESTER} end
+	if SERVER and JESTER then self.networkRoles = {JESTER} end
 end
 
 if SERVER then
-  function ROLE:GiveRoleLoadout(ply, isRoleChange)
-    ply:SetNWFloat("Leech_Hunger_Level", GetConVar("ttt2_leech_starve_time"):GetFloat())
-    ply.leechHungerTime = CurTime() + 0.04
-    ply.leechChange = -(GetConVar("ttt2_leech_tick_length"):GetFloat())
-  end
+	function ROLE:GiveRoleLoadout(ply, isRoleChange)
+		ply:SetNWFloat("Leech_Hunger_Level", GetConVar("ttt2_leech_starve_time"):GetFloat())
+		ply.leechHungerTime = CurTime() + 0.04
+		ply.leechChange = -(GetConVar("ttt2_leech_tick_length"):GetFloat())
+	end
 
-  hook.Add("TTT2ModifyWinningAlives", "CheckLeechTeamSwitch", function(alives)
-    local winningTeam = ""
+	hook.Add("TTT2ModifyWinningAlives", "CheckLeechTeamSwitch", function(alives)
+		local winningTeam = ""
 
-    if alives == nil then return end
+		if alives == nil then return end
 
-    -- Check alive teams
-    for i in pairs(alives) do
-      local t = alives[i]
-      if winningTeam != "" and winningTeam != t then return end
-      winningTeam = t
-    end
+		-- Check alive teams
+		for i in pairs(alives) do
+			local t = alives[i]
+			if winningTeam ~= "" and winningTeam ~= t then return end
+			winningTeam = t
+		end
 
-    if winningTeam == "" then return end
- 
-    -- Find any Leechs that arn't feeding and prevent a win
-    for _, ply in ipairs(player.GetAll()) do
-      if not IsValid(ply) or not ply:Alive() then continue end
-      if SpecDM and (ply.IsGhost and ply:IsGhost()) then continue end
+		if winningTeam == "" then return end
 
-      if ply:GetSubRole() == ROLE_LEECH and ply.leechChange < 0 then
-        table.insert(alives, "leech")
-        return
-      end
-    end
+		-- Find any Leechs that arn't feeding and prevent a win
+		for _, ply in ipairs(player.GetAll()) do
+			if not IsValid(ply) or not ply:Alive() then continue end
+			if SpecDM and (ply.IsGhost and ply:IsGhost()) then continue end
 
-  -- Find all Leechs and turn them to the winning team
-    for _, ply in ipairs(player.GetAll()) do
-      if not IsValid(ply) or not ply:Alive() then continue end
+			if ply:GetSubRole() == ROLE_LEECH and ply.leechChange < 0 then
+				table.insert(alives, "leech")
+				return
+			end
+		end
+
+	-- Find all Leechs and turn them to the winning team
+		for _, ply in ipairs(player.GetAll()) do
+			if not IsValid(ply) or not ply:Alive() then continue end
 			if SpecDM and (ply.IsGhost and ply:IsGhost() or (vics.IsGhost and vics:IsGhost())) then continue end
 
-      if ply:GetSubRole() == ROLE_LEECH and ply.leechChange >= 0 then
-        ply:UpdateTeam(winningTeam, false)
-      end
-    end
-  end)
+			if ply:GetSubRole() == ROLE_LEECH and ply.leechChange >= 0 then
+				ply:UpdateTeam(winningTeam, false)
+			end
+		end
+	end)
 
-  hook.Add("Think", "LeechHungerThink", function()
-    if GetRoundState() ~= ROUND_ACTIVE then return end
+	hook.Add("Think", "LeechHungerThink", function()
+		if GetRoundState() ~= ROUND_ACTIVE then return end
 
-    -- Get all viable host positions
-    local hostPoses = {}
+		-- Get all viable host positions
+		local hostPoses = {}
 
-    for _, ply in ipairs(player.GetAll()) do
-      if not IsValid(ply) or not ply:Alive() or ply:IsSpec() then continue end
+		for _, ply in ipairs(player.GetAll()) do
+			if not IsValid(ply) or not ply:Alive() or ply:IsSpec() then continue end
 			if SpecDM and (ply.IsGhost and ply:IsGhost() or (vics.IsGhost and vics:IsGhost())) then continue end
-      if ply:GetSubRole() == ROLE_LEECH then continue end
+			if ply:GetSubRole() == ROLE_LEECH then continue end
 
-      table.insert(hostPoses, ply:GetPos())
-    end
+			table.insert(hostPoses, ply:GetPos())
+		end
 
-    -- Foreach leech
-    for _, ply in ipairs(player.GetAll()) do
-      if not IsValid(ply) or not ply:Alive() or ply:IsSpec() then continue end
+		-- Foreach leech
+		for _, ply in ipairs(player.GetAll()) do
+			if not IsValid(ply) or not ply:Alive() or ply:IsSpec() then continue end
 			if SpecDM and (ply.IsGhost and ply:IsGhost() or (vics.IsGhost and vics:IsGhost())) then continue end
-      if ply:GetSubRole() ~= ROLE_LEECH then continue end
+			if ply:GetSubRole() ~= ROLE_LEECH then continue end
 
-      if not ply.leechHungerTime then ply.leechHungerTime = CurTime() + GetConVar("ttt2_leech_tick_length"):GetFloat() end
+			if not ply.leechHungerTime then ply.leechHungerTime = CurTime() + GetConVar("ttt2_leech_tick_length"):GetFloat() end
 
-      local closestDistance = 1000000.0
-      for i, hostPos in ipairs(hostPoses) do
-        if ply:GetPos():DistToSqr(hostPos) < closestDistance then closestDistance = ply:GetPos():DistToSqr(hostPos) end
-      end
+			local closestDistance = 1000000.0
+			for i, hostPos in ipairs(hostPoses) do
+				if ply:GetPos():DistToSqr(hostPos) < closestDistance then closestDistance = ply:GetPos():DistToSqr(hostPos) end
+			end
 
-      if ply.leechHungerTime <= CurTime() then
-         ply.leechChange = -(GetConVar("ttt2_leech_tick_length"):GetFloat())
-        if closestDistance < GetConVar("ttt2_leech_refill_radius"):GetFloat() then
-          ply.leechChange = GetConVar("ttt2_leech_tick_length"):GetFloat() * GetConVar("ttt2_leech_refill_multiplier"):GetFloat()
-        end
+			if ply.leechHungerTime <= CurTime() then
+				 ply.leechChange = -(GetConVar("ttt2_leech_tick_length"):GetFloat())
+				if closestDistance < GetConVar("ttt2_leech_refill_radius"):GetFloat() then
+					ply.leechChange = GetConVar("ttt2_leech_tick_length"):GetFloat() * GetConVar("ttt2_leech_refill_multiplier"):GetFloat()
+				end
 
-        ply:SetNWFloat("Leech_Hunger_Level", math.Clamp(ply:GetNWFloat("Leech_Hunger_Level", 0) + ply.leechChange, 0, GetConVar("ttt2_leech_starve_time"):GetFloat()))
+				ply:SetNWFloat("Leech_Hunger_Level", math.Clamp(ply:GetNWFloat("Leech_Hunger_Level", 0) + ply.leechChange, 0, GetConVar("ttt2_leech_starve_time"):GetFloat()))
 
-        if ply:GetNWFloat("Leech_Hunger_Level") > 0 then
-            ply.leechHungerTime = CurTime() + GetConVar("ttt2_leech_tick_length"):GetFloat()
-        else
-            ply:Kill()
-        end
-      end
-    end
-  end)
+				if ply:GetNWFloat("Leech_Hunger_Level") > 0 then
+						ply.leechHungerTime = CurTime() + GetConVar("ttt2_leech_tick_length"):GetFloat()
+				else
+						ply:Kill()
+				end
+			end
+		end
+	end)
+end
+
+if CLIENT then
+	function ROLE:AddToSettingsMenu(parent)
+		local form = vgui.CreateTTT2Form(parent, "header_roles_additional")
+
+		form:MakeHelp({
+			label = "desc_ttt2_leech_refill_radius"
+		})
+		form:MakeSlider({
+			serverConvar = "ttt2_leech_refill_radius",
+			label = "label_ttt2_leech_refill_radius",
+			min = 0,
+			max = 1000000,
+			decimal = 0,
+		})
+
+		form:MakeHelp({
+			label = "desc_ttt2_leech_refill_multiplier"
+		})
+		form:MakeSlider({
+			serverConvar = "ttt2_leech_refill_multiplier",
+			label = "label_ttt2_leech_refill_multiplier",
+			min = 0,
+			max = 100,
+			decimal = 2,
+		})
+
+		form:MakeHelp({
+			label = "desc_ttt2_leech_starve_time"
+		})
+		form:MakeSlider({
+			serverConvar = "ttt2_leech_starve_time",
+			label = "label_ttt2_leech_starve_time",
+			min = 0,
+			max = 100,
+			decimal = 2,
+		})
+
+		form:MakeHelp({
+			label = "desc_ttt2_leech_tick_length"
+		})
+		form:MakeSlider({
+			serverConvar = "ttt2_leech_tick_length",
+			label = "label_ttt2_leech_tick_length",
+			min = 0,
+			max = 1,
+			decimal = 2,
+		})
+	end
 end
--- lua/terrortown/lang/en/leech.lua
+++ lua/terrortown/lang/en/leech.lua
@@ -6,3 +6,12 @@ L["body_found_" .. LEECH.abbr] = "They were a Leech!"
 L["search_role_" .. LEECH.abbr] = "This person was a Leech!"
 L["target_" .. LEECH.name] = "Leech"
 L["ttt2_desc_" .. LEECH.name] = [[Leech is a neutral role that will die if alone for too long, and wins with the winning team if they remain alive.]]
+
+L.label_ttt2_leech_refill_radius = "Refill Radius"
+L.desc_ttt2_leech_refill_radius = "The maximum (squared) distance a player can be from a Leech to prevent starving."
+L.label_ttt2_leech_refill_multiplier = "Refill Multiplier"
+L.desc_ttt2_leech_refill_multiplier = "The how much hunger is satiated every tick. This is a factor of the \"Hunger Tick Length\"."
+L.label_ttt2_leech_starve_time = "Starve Time"
+L.desc_ttt2_leech_starve_time = "The duration of time that a Leech can remain isolated before taking health damage."
+L.label_ttt2_leech_tick_length = "Hunger Tick Length"
+L.desc_ttt2_leech_tick_length = "The amount of time that passes between each hunger check."
